c3fa6f40fa96d03c3ab89bf7c6071d203a29e4ff
Audrey Phung <audreyphung8@gmail.com>
2025-07-07 22:44:57 -0700

    added start page

--- src/App.jsx
+++ src/App.jsx
@@ -40,10 +40,11 @@ function App() {
   const [openModal, setOpen] = React.useState(false);
 
 
+
   const handleOpen = () => setOpen(true);
   const closeModal = () => setOpen(false);
 
-  const handleClick = (event) => 
+  const handlePopup = (event) => 
   {
     setAnchorEl(event.currentTarget);
   };
@@ -59,7 +60,7 @@ function App() {
     const tempValue = quantityCount.slice();
 
     {/* Makes sure that the specific fruit (which is tracked by index) is updated and clicks cannot exceed 10*/}
-    if (quantityCount[index] < 10)
+    if (quantityCount[index] < 99)
     {
       tempValue[index] = quantityCount[index] + 1;
       setQuantityCount(tempValue);
@@ -152,137 +153,178 @@ function App() {
   }
 
   return (
-    <>  
-      <Typography variant="h1" display="flex" justifyContent="center" sx={{ bgcolor:'#fce5dd', width: 'fit-content', borderRadius: 2, px: 4, textAlign: 'center', mx: 'auto', mb: 3, ml: 55}} alignItems="center">
-        Fruit Basket!
-       </Typography>
+    <> 
+      {!trigger &&
+        <Box display="flex" justifyContent="center" alignItems="center" sx={{ bgcolor: '#fce5dd', width: 'fit-content', borderRadius: 2, px: 4, mx: 'auto', mb: 3}}>     
+          <Typography variant="h1" sx={{ textAlign: 'center', mr: 2, fontFamily: 'Gambetta',fontWeight: 'bold' }}>
+            Fruit Basket!
+          </Typography>
+          <img src="https://cdn-icons-png.flaticon.com/512/5601/5601378.png" width="100"></img>
+        </Box>
+      }
 
-      <Box display="flex" bgcolor='#fce5dd' justifyContent="center" alignItems="center" marginLeft={30} flexDirection="column" sx={{ borderRadius: 7, p: 4}}>
-        
-        <FormGroup row sx={{columnGap: 4}}>
-          <FormControlLabel control={<Checkbox onChange={handleChange(0)}/>}
-            label=
-            {
-              <img src="https://www.iconpacks.net/icons/2/free-apple-icon-3155-thumb.png" alt="apple icon" width="150"/>
-            }
-          />
+      {!trigger &&
+        <Box display="flex" bgcolor='#fce5dd' justifyContent="center" alignItems="center" marginLeft={20} marginRight={15} flexDirection="column" sx={{ borderRadius: 7, p: 4}}>
+          <Typography variant="h4" sx={{fontFamily: 'Recia'}}>
+            Keep your basket within 70 grams of sugar to stay within the daily limit!
+          </Typography>
 
-          <FormControlLabel control={<Checkbox onChange={handleChange(1)}/>}
-            label=
-            {
-              <img src="https://cdn-icons-png.flaticon.com/512/6482/6482627.png" alt="banana icon"  width="150"/>
-            }
-          /> 
-
-          <FormControlLabel control={<Checkbox onChange={handleChange(2)}/>}
-            label=
-            {
-              <img src="https://cdn-icons-png.flaticon.com/512/5210/5210447.png" alt="strawberry icon"  width="150"/>
-            }
-          />
-
-          <FormControlLabel control={<Checkbox onChange={handleChange(3)}/>}
-            label=
-            {
-              <img src="https://cdn-icons-png.flaticon.com/512/5210/5210493.png" alt="blueberry icon"  width="150"/>
-            }
-          /> 
-        </FormGroup>
-        
-        {/* https://stackoverflow.com/questions/59305603/increment-and-decrement-button-via-material-ui-buttongroup */}
-        <ButtonGroup size="large" aria-label="small outlined button group" sx={{ marginTop: 2}}>
-          <Button variant="outlined" onClick={handleDecrement(0)}>-</Button>
-          <Button display="true">{quantityCount[0]}</Button>
-          <Button variant="outlined" onClick={handleIncrement(0)} sx={{marginRight:10}}>+</Button>
-
-          <Button variant="outlined" onClick={handleDecrement(1)}>-</Button>
-          <Button display="true">{quantityCount[1]}</Button>
-          <Button variant="outlined" onClick={handleIncrement(1)}sx={{marginRight:10}}>+</Button>
-
-          <Button variant="outlined" onClick={handleDecrement(2)}>-</Button>
-          <Button display="true">{quantityCount[2]}</Button>
-          <Button variant="outlined" onClick={handleIncrement(2)} sx={{marginRight:10}}>+</Button>
-
-          <Button variant="outlined" onClick={handleDecrement(3)}>-</Button>
-          <Button display="true">{quantityCount[3]}</Button>
-          <Button variant="outlined" onClick={handleIncrement(3)} sx={{marginRight:10}}>+</Button>
-        </ButtonGroup>
-        
-        {/*Area of struggle: Accessing a value while it is getting fetched async, not getting the updated value immediately 
-        Solution: Created a button to allow it to finish processing before I utilize the values in conditional statements
-        When I console.log, I noticed that values were getting updated PER fetch since it has to fetch for each fruit which means that 
-        it has to wait until its done before it can be used*/}
-        <Button variant="outlined" sx= {{
-          mt: 7,
-          mr: 20,
-          borderColor: 'black',
-          color: 'black',
-          '&:hover': {
-            backgroundColor: '#fa5081',
-          },
-        }}
-        endIcon={<SaveAltTwoToneIcon/>} 
-          onClick={event => {fetchFruit()}}>
-          Save
-        </Button>
-
-        <Button variant="contained" sx= {{
-          mt: -4.5,
-          ml: 14,
-          backgroundColor: '#fa5081',       
-          color: 'black',                
-          borderColor: 'black',          
-          border: '1px solid black',     
-          '&:hover': {
-            backgroundColor: 'white',
-          },
-        }}
-        
-        endIcon={<ArrowForwardIcon/>} 
-
-          onClick={event => {
-            if (count > 70)
-            {
-              handleClick(event);
-              setCount(0);
-            }
-            else
-            {
-              handleOpen();
-            }
-          }}>
-          Confirm
-        </Button>
-
-        <Popover
-          id={id}
-          open={open}
-          anchorEl={anchorEl}
-          onClose={handleClose}
-          anchorOrigin={{
-            vertical: 'center',
-            horizontal: 'center',}}
-        >
-          <Typography sx={{p: 2}}>That's too much sugar!</Typography>
-        </Popover>
-
-        <Modal
-          open={openModal}
-          onClose={closeModal}>
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Your fruit bowl has {count}g of sugar! 
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              This is very healthy, but remember that too much sugar can be bad for you.
-            </Typography>
+          <Box sx={{mr:100, mb: -15}}>
+            <img src="https://sdk.bitmoji.com/render/panel/20053424-150807873_37-s5-v1.png?transparent=1&palette=1&scale=2" alt="my bitmoji" width="200"/>
           </Box>
-        </Modal>
+          <Typography component="ul" sx={{fontSize: '25px', ml: 30, mt: -5, fontFamily: 'General Serif'}}>
+            <li >Click a fruit to start earning points</li>
+            <li>After you have chosen a fruit, you can increase or decrease the quantity</li>
+            <li>Don't forget to hit save then confirm!</li>
+          </Typography>
+          
+          <Button sx={{mt: 3, 
+            backgroundColor: '#fa5081',       
+            color: 'black',                
+            borderColor: 'black',          
+            border: '1px solid black',     
+            '&:hover': {
+              backgroundColor: 'white',
+            },}}variant="outlined" onClick={() => setTrigger(true)}>
+            Start
+          </Button>
+        </Box>
+      }
 
-      </Box>
+      {trigger &&
+        <Box display="flex" justifyContent="center" alignItems="center" sx={{ bgcolor: '#fce5dd', width: 'fit-content', borderRadius: 2, px: 4, mx: 'auto', mb: 3, mr: 18}}>     
+          <Typography variant="h1" sx={{ textAlign: 'center', mr: 2, fontFamily: 'Gambetta',fontWeight: 'bold' }}>
+            Fruit Basket!
+          </Typography>
+          <img src="https://cdn-icons-png.flaticon.com/512/5601/5601378.png" width="100"></img>
+        </Box>
+      }
+      {trigger &&
+          <Box display="flex" bgcolor='#fce5dd' justifyContent="center" alignItems="center" marginLeft={30} flexDirection="column" sx={{ borderRadius: 7, p: 4}}>
+          
+          <FormGroup row sx={{columnGap: 4}}>
+            <FormControlLabel control={<Checkbox onChange={handleChange(0)}/>}
+              label=
+              {
+                <img src="https://www.iconpacks.net/icons/2/free-apple-icon-3155-thumb.png" alt="apple icon" width="150"/>
+              }
+            />
 
+            <FormControlLabel control={<Checkbox onChange={handleChange(1)}/>}
+              label=
+              {
+                <img src="https://cdn-icons-png.flaticon.com/512/6482/6482627.png" alt="banana icon"  width="150"/>
+              }
+            /> 
+
+            <FormControlLabel control={<Checkbox onChange={handleChange(2)}/>}
+              label=
+              {
+                <img src="https://cdn-icons-png.flaticon.com/512/5210/5210447.png" alt="strawberry icon"  width="150"/>
+              }
+            />
+
+            <FormControlLabel control={<Checkbox onChange={handleChange(3)}/>}
+              label=
+              {
+                <img src="https://cdn-icons-png.flaticon.com/512/5210/5210493.png" alt="blueberry icon"  width="150"/>
+              }
+            /> 
+          </FormGroup>
+          
+          {/* https://stackoverflow.com/questions/59305603/increment-and-decrement-button-via-material-ui-buttongroup */}
+          <ButtonGroup size="large" aria-label="small outlined button group" sx={{ marginTop: 2}}>
+            <Button variant="outlined" onClick={handleDecrement(0)}>-</Button>
+            <Button display="true">{quantityCount[0]}</Button>
+            <Button variant="outlined" onClick={handleIncrement(0)} sx={{marginRight:10}}>+</Button>
+
+            <Button variant="outlined" onClick={handleDecrement(1)}>-</Button>
+            <Button display="true">{quantityCount[1]}</Button>
+            <Button variant="outlined" onClick={handleIncrement(1)}sx={{marginRight:10}}>+</Button>
+
+            <Button variant="outlined" onClick={handleDecrement(2)}>-</Button>
+            <Button display="true">{quantityCount[2]}</Button>
+            <Button variant="outlined" onClick={handleIncrement(2)} sx={{marginRight:10}}>+</Button>
+
+            <Button variant="outlined" onClick={handleDecrement(3)}>-</Button>
+            <Button display="true">{quantityCount[3]}</Button>
+            <Button variant="outlined" onClick={handleIncrement(3)} sx={{marginRight:10}}>+</Button>
+          </ButtonGroup>
+          
+          {/*Area of struggle: Accessing a value while it is getting fetched async, not getting the updated value immediately 
+          Solution: Created a button to allow it to finish processing before I utilize the values in conditional statements
+          When I console.log, I noticed that values were getting updated PER fetch since it has to fetch for each fruit which means that 
+          it has to wait until its done before it can be used*/}
+          <Button variant="outlined" sx= {{
+            mt: 7,
+            mr: 20,
+            borderColor: 'black',
+            color: 'black',
+            '&:hover': {
+              backgroundColor: '#fa5081',
+            },
+          }}
+          endIcon={<SaveAltTwoToneIcon/>} 
+            onClick={event => {fetchFruit()}}>
+            Save
+          </Button>
+
+          <Button variant="contained" sx= {{
+            mt: -4.5,
+            ml: 14,
+            backgroundColor: '#fa5081',       
+            color: 'black',                
+            borderColor: 'black',          
+            border: '1px solid black',     
+            '&:hover': {
+              backgroundColor: 'white',
+            },
+          }}
+          
+          endIcon={<ArrowForwardIcon/>} 
+
+            onClick={event => {
+              if (count > 70)
+              {
+                handlePopup(event);
+                setCount(0);
+              }
+              else
+              {
+                handleOpen();
+              }
+            }}>
+            Confirm
+          </Button>
+
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'center',
+              horizontal: 'center',}}
+          >
+            <Typography sx={{p: 2}}>That's too much sugar!</Typography>
+          </Popover>
+
+          <Modal
+            open={openModal}
+            onClose={closeModal}>
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Your fruit bowl has {count}g of sugar! 
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                This is very healthy, but remember that too much sugar can be bad for you.
+              </Typography>
+            </Box>
+          </Modal>
+
+        </Box>
+      }
     </>
   );
 }
-
 export default App
